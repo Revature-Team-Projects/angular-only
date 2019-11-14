eb3d73c0bfd53e15053338b524f436d19ea45269
Dilshan <dilshan.diunugala@outlook.com>
2019-11-14 14:30:50 -0500

    testing with headless browser

--- angularTest/karma.conf.js
+++ angularTest/karma.conf.js
@@ -12,22 +12,28 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless', '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222']
+      }
+    },
+
+    browsers: ['ChromeHeadless'],
+    singleRun: true
   });
 };
