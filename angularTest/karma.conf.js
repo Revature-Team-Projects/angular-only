f70c5f909ccbf83af5ce09d0dbf3fa1fdc8d7f9f
doriapameni <doriapameni@gmail.com>
2019-11-14 22:26:27 -0500

    fixing karma conf file

--- angularTest/karma.conf.js
+++ angularTest/karma.conf.js
@@ -1,44 +1,40 @@
-const os = require('os');
-const chromeHeadlessSupported = os.platform() !== 'win32' || Number((os.release().match(/^(\d+)/) || ['0', '0'])[1]) >= 10;
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/0.13/config/configuration-file.html
+
+// process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('karma-coverage'),
-      require('karma-mocha-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular/cli/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['mocha','coverage'],
-    coverageReporter: {
-      type: 'text-summary'
+    angularCli: {
+      environment: 'dev'
     },
-    port: 9876,
+    reporters: ['progress', 'kjhtml'],
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browserNoActivityTimeout: 30000,
-    browsers: [
-      chromeHeadlessSupported ? 'ChromeHeadless' : 'Chrome'
-    ],
+    browsers: ["Chrome", "HeadlessChrome"],
     customLaunchers: {
-      ChromeHeadless: {
-          base: 'Chrome',
-          flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
-      }
+        HeadlessChrome: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"]
+        }
     },
-    singleRun: true
   });
 };
