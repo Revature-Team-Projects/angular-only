a28c2a4aa7df71d7601272a06f0a4590a7c1261a
Dilshan <dilshan.diunugala@outlook.com>
2019-11-19 11:54:28 -0500

    Added jenkinsfile, with changes to package.json script change

---
+++ RideShare/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent},
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
+export const routing = RouterModule.forRoot(appRoutes);
